ae390269c4b33af97d961e6d3ae6628ebe0fb6c1
arseo <ahrim2020@gmail.com>
2019-04-21 22:23:09 +0900

    [2019-04-21-arseo]
    1. 부트스트랩 적용 (경로 : resources/bootstrap)
    2. 템플릿 적용 (경로 : resources/templates)
    3. 템플릿 기존 소스 html 모두 jsp로 바꿈 (html 경로 : views/template_html)
    4. 컨트롤러 없이 페이지 요청하게 HomeController.java 수정
    5. 실행 url : localhost:8080/airlc/home

--- airlc/src/main/java/com/drimin/airlc/HomeController.java
+++ airlc/src/main/java/com/drimin/airlc/HomeController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -18,11 +19,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+
+	/*원본임*/
+	/*@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
@@ -34,6 +33,12 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
+	}*/
+	
+	/*컨트롤러 없이 페이지 이동*/
+	@RequestMapping(value = "/{pageName}", method = RequestMethod.GET)
+	public String getSinglePage(@PathVariable("pageName")String pageName) {
+		return "/"+pageName;
 	}
 	
 }
